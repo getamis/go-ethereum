58a0a696fa428914bbeb93d9abf9a469bd6f6dd5
Edwin <edwin@maicoin.com>
2017-04-14 16:50:19 +0800

    consensus/pbft: add APIs for PBFT

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -30,9 +30,8 @@ var Modules = map[string]string{
 	"shh":        Shh_JS,
 	"swarmfs":    SWARMFS_JS,
 	"txpool":     TxPool_JS,
-
+	"pbft":       PBFT_JS,
 }
-
 
 const Chequebook_JS = `
 web3._extend({
@@ -575,6 +574,28 @@ web3._extend({
 				return status;
 			}
 		})
+	]
+});
+`
+
+const PBFT_JS = `
+web3._extend({
+  	property: 'pbft',
+  	methods:
+  	[
+		new web3._extend.Method({
+			name: 'consensusState',
+			call: 'pbft_consensusState',
+			params: 0
+		}),
+		new web3._extend.Method({
+			name: 'viewChange',
+			call: 'pbft_viewChange',
+			params: 0
+		}),
+  	],
+	properties:
+	[
 	]
 });
 `
