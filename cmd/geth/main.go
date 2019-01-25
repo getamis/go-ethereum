1806470b6bfa98d5705383d5c860cd6a7978a4b9
yysung <yysung@maicoin.com>
2019-01-25 14:26:35 +0800

    *: add broadcast local txs feature

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -71,6 +71,7 @@ var (
 		utils.TxPoolNoLocalsFlag,
 		utils.TxPoolJournalFlag,
 		utils.TxPoolRejournalFlag,
+		utils.TxPoolBroadcastPendingLocalTxFlag,
 		utils.TxPoolPriceLimitFlag,
 		utils.TxPoolPriceBumpFlag,
 		utils.TxPoolAccountSlotsFlag,
