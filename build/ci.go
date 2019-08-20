3ea467bcba1d121c111f253e685e3e58f6a6ae1e
mark.lin <mark@maicoin.com>
2019-08-20 13:22:58 +0800

    .travis: only build indexer images

--- build/ci.go
+++ build/ci.go
@@ -304,7 +304,7 @@ func doTest(cmdline []string) {
 	gotest := tc.Go("test")
 
 	// CI needs a bit more time for the statetests (default 10m).
-	gotest.Args = append(gotest.Args, "-timeout=30m")
+	gotest.Args = append(gotest.Args, "-timeout=120m")
 
 	// Enable CKZG backend in CI.
 	gotest.Args = append(gotest.Args, "-tags=ckzg")
