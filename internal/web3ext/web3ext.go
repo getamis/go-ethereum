c8fb011447f3a07beb62124146fc9ecdce95b04d
mark.lin <mark@maicoin.com>
2019-08-20 14:25:02 +0800

    *: add GetTransferLogs/GetBlockReceipts/SubscribeNewQueuedTxsEvent apis

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -468,6 +468,18 @@ web3._extend({
 			call: 'debug_getTrieFlushInterval',
 			params: 0
 		}),
+		new web3._extend.Method({
+			name: 'getTransferLogs',
+			call: 'debug_getTransferLogs',
+			params: 1,
+			inputFormatter: [null],
+		}),
+		new web3._extend.Method({
+			name: 'getBlockReceipts',
+			call: 'debug_getBlockReceipts',
+			params: 1,
+			inputFormatter: [null],
+		}),
 	],
 	properties: []
 });
