47ed7c9fc5ab616c894871fe973e21cd3b0252b1
Miya Chen <miyatlchen@gmail.com>
2020-02-11 13:35:31 +0800

    core, eth: refactor read/write transfer logs

--- eth/api_debug.go
+++ eth/api_debug.go
@@ -447,11 +447,7 @@ func (api *DebugAPI) GetTrieFlushInterval() (string, error) {
 
 // GetTransferLogs is a debug API function that returns the transfer logs for a block hash, if known.
 func (api *DebugAPI) GetTransferLogs(ctx context.Context, hash common.Hash) ([]*types.TransferLog, error) {
-	number := rawdb.ReadHeaderNumber(api.eth.ChainDb(), hash)
-	if number == nil {
-		return nil, errors.New("unknown transfer logs")
-	}
-	if transferLogs := rawdb.ReadTransferLogs(api.eth.ChainDb(), hash, *number); transferLogs != nil {
+	if transferLogs := api.eth.blockchain.GetTransferLogs(hash); transferLogs != nil {
 		return transferLogs, nil
 	}
 	return nil, errors.New("unknown transfer logs")
@@ -463,6 +459,9 @@ func (api *DebugAPI) GetTotalDifficulty(blockHash common.Hash) *big.Int {
 }
 
 // GetBlockReceipts returns all transaction receipts of the specified block.
-func (api *DebugAPI) GetBlockReceipts(blockHash common.Hash) types.Receipts {
-	return api.eth.blockchain.GetReceiptsByHash(blockHash)
+func (api *DebugAPI) GetBlockReceipts(blockHash common.Hash) (types.Receipts, error) {
+	if receipts := api.eth.blockchain.GetReceiptsByHash(blockHash); receipts != nil {
+		return receipts, nil
+	}
+	return nil, errors.New("unknown receipts")
 }
