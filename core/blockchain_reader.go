1aaf3b89fff74ec7a3d19d15073dddb03725c02d
Miya Chen <miyatlchen@gmail.com>
2020-02-12 13:11:05 +0800

    core,core/rawdb, eth: return error when getting transfer logs

--- core/blockchain_reader.go
+++ core/blockchain_reader.go
@@ -19,6 +19,8 @@ package core
 import (
 	"errors"
 
+	"github.com/ethereum/go-ethereum"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -456,10 +458,15 @@ func (bc *BlockChain) SubscribeBlockProcessingEvent(ch chan<- bool) event.Subscr
 }
 
 // GetTransferLogs retrieves the transfer logs for all transactions in a given block.
-func (bc *BlockChain) GetTransferLogs(hash common.Hash) []*types.TransferLog {
+func (bc *BlockChain) GetTransferLogs(hash common.Hash) ([]*types.TransferLog, error) {
 	number := rawdb.ReadHeaderNumber(bc.db, hash)
 	if number == nil {
-		return nil
+		return nil, ethereum.NotFound
 	}
-	return rawdb.ReadTransferLogs(bc.db, hash, *number)
+	transferLogs, err := rawdb.ReadTransferLogs(bc.db, hash, *number)
+	if err != nil {
+		return nil, err
+	}
+	bc.transferLogsCache.Add(hash, transferLogs)
+	return transferLogs, nil
 }
