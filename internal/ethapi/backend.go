f2da966796ea0cd6a2d69c4de176bc67ff3fbde6
Jason Paryani <github@jparyani.com>
2022-09-10 14:05:14 -0700

    flashbots: Add eth_callBundle and eth_estimateGasBundle

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -100,7 +100,7 @@ type Backend interface {
 	ServiceFilter(ctx context.Context, session *bloombits.MatcherSession)
 }
 
-func GetAPIs(apiBackend Backend) []rpc.API {
+func GetAPIs(apiBackend Backend, chain *core.BlockChain) []rpc.API {
 	nonceLock := new(AddrLocker)
 	return []rpc.API{
 		{
@@ -124,6 +124,9 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 		}, {
 			Namespace: "personal",
 			Service:   NewPersonalAccountAPI(apiBackend, nonceLock),
+		}, {
+			Namespace: "eth",
+			Service:   NewBundleAPI(apiBackend, chain),
 		},
 	}
 }
